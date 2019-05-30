fb42b196757ca96ae9d311c7acec4db65cd89e01
17010233 <17010233@myrp.edu.sg>
2019-05-30 12:24:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a17010233/demonotification/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.a17010233.demonotification;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    int requestCode = 123;
+    int notificationID = 888;
+    Button btnNotify1, btnNotify2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnNotify1 = findViewById(R.id.btnNotify1);
+        btnNotify2 = findViewById(R.id.btnNotify2);
+
+
+        btnNotify1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                NotificationManager notificationManager = (NotificationManager)
+                        getSystemService(NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel("default", "Default Channel",
+                            NotificationManager.IMPORTANCE_HIGH);
+
+                    channel.setDescription("This is for default notification");
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pIntent = PendingIntent.getActivity
+                        (MainActivity.this, requestCode, intent,
+                                PendingIntent.FLAG_CANCEL_CURRENT);
+
+                // Build notification
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "default");
+                builder.setContentTitle("Amazing Offer!");
+                builder.setContentText("Subject");
+                builder.setSmallIcon(android.R.drawable.btn_star_big_off);
+                builder.setContentIntent(pIntent);
+                builder.setAutoCancel(true);
+
+                //builder.setPriority(Notification.PRIORITY_HIGH);
+
+                Notification n = builder.build();
+
+                // An integer good to have, for you to programmatically cancel it
+                notificationManager.notify(notificationID, n);
+                finish();
+            }
+        });
+
+        btnNotify2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                NotificationManager notificationManager = (NotificationManager)
+                        getSystemService(NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new
+                            NotificationChannel("default", "Default Channel",
+                            NotificationManager.IMPORTANCE_DEFAULT);
+
+                    channel.setDescription("This is for default notification");
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pIntent = PendingIntent.getActivity
+                        (MainActivity.this, requestCode,
+                                intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                NotificationCompat.BigTextStyle bigText = new NotificationCompat.BigTextStyle();
+                bigText.setBigContentTitle("Big Text – Long Content");
+                String msg ="This is one big text" +
+                        " - A quick brown fox jumps over a lazy brown dog " +
+                        "\nLorem ipsum dolor sit amet, sea eu quod des";
+
+//                bigText.bigText("This is one big text" +
+//                        " - A quick brown fox jumps over a lazy brown dog " +
+//                        "\nLorem ipsum dolor sit amet, sea eu quod des");
+
+                bigText.setSummaryText("Reflection Journal?");
+
+                // Build notification
+                Notification.Builder builder = new Notification.Builder(MainActivity.this, "default");
+                builder.setContentTitle("Amazing Offer!");
+                builder.setContentText("Subject");
+                builder.setSmallIcon(android.R.drawable.btn_star_big_off);
+                builder.setContentIntent(pIntent);
+                builder.setStyle(new Notification.BigTextStyle().bigText(msg));
+                builder.setAutoCancel(true);
+
+                Notification n = builder.build();
+
+                // This replaces the existing notification with the same ID
+                notificationManager.notify(notificationID, n);
+                finish();
+            }
+        });
+    }
+
+
+}
